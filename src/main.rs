e513db611f81fa0a11ee1fa1ced619bf3680b83b
p0lyh3dron <thep0lyh3dron@gmail.com>
2026-01-11 00:12:46 -0500

    Main: Add Graphics test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     let mut script = creek::api::Scripts::new("lua");
     let _ = script.exec("print('Hello, Creek!');");
+
+    let mut graphics = creek::api::Graphics::new("dummy");
 }
