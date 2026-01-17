2faeb198547e95fde3c841fc42e2f8884f685b0f
p0lyh3dron <thep0lyh3dron@gmail.com>
2026-01-16 21:18:13 -0500

    General: Prepare for governing api interfacing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ fn main() {
     let mut graphics = creek::api::Graphics::new("vulkan");
     graphics.init();
 
+    let mut governor = creek::api::Governor::new("winit");
+    governor.init();
+
+    governor.submit();
+
     loop {
         _ = script.exec("update();");
 
