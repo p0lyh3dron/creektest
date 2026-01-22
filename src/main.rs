eeae019334649858889c0ee8051457fd3f0c03be
p0lyh3dron <thep0lyh3dron@gmail.com>
2026-01-21 21:28:08 -0500

    General: Fit to changes in creek

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,10 @@ fn main() {
     let mut script = creek::api::Scripts::new("lua");
     let _ = script.exec("require('main'); init();");
 
-    let mut graphics = creek::api::Graphics::new("vulkan");
-    graphics.init();
-
-    let mut governor = creek::api::Governor::new("winit");
-    governor.init();
-
-    governor.submit();
+    let mut io = creek::api::IO::new("vulkan");
+    io.init();
 
     loop {
         _ = script.exec("update();");
-
-        graphics.update();
     }
 }
