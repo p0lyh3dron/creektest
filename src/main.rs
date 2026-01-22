175ce672ef871745fd7230180e92abddf6656b06
p0lyh3dron <thep0lyh3dron@gmail.com>
2026-01-21 21:53:10 -0500

    Main: Move app logic to another thread

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ fn main() {
     let mut script = creek::api::Scripts::new("lua");
     let _ = script.exec("require('main'); init();");
 
+    std::thread::spawn(move || {
+        loop {
+            _ = script.exec("update();");
+        }
+    });
+
     let mut io = creek::api::IO::new("vulkan");
     io.init();
-
-    loop {
-        _ = script.exec("update();");
-    }
 }
