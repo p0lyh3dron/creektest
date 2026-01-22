b8b9f7db9b2dedd8d0566860d7f587df7aff5f58
p0lyh3dron <thep0lyh3dron@gmail.com>
2026-01-21 22:57:56 -0500

    Main: Add audio test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
+    let mut audio = creek::api::Audio::new("rodio");
+
     let mut script = creek::api::Scripts::new("lua");
     let _ = script.exec("require('main'); init();");
 
+    let sound = audio.load_sound_file("../creek/assets/sound.wav").unwrap();
     std::thread::spawn(move || {
+        std::thread::sleep(std::time::Duration::from_millis(350));
+        audio.play_sound(sound);
         loop {
             _ = script.exec("update();");
         }
